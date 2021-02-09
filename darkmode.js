f557330c041e3e27d94130e65ebb845deaf5e2cf
Gustavo Felipe Batista Carneiro <gustavo.felipebc@gmail.com>
2021-02-09 11:54:31 -0300

    implements new version of darkmode and method of save in localStorage

--- darkmode.js
+++ darkmode.js
@@ -1,39 +1,68 @@
-function darkmode(){
-    document.querySelector('body').classList.toggle('body-dark');
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeStorage = localStorage.getItem('dark-mode');
+    const html = document.querySelector('html');
+    const inputDarkMode = document.getElementById('input-dark-mode');
 
-    document.querySelector('header').classList.toggle('header-dark');
-    
-    document.querySelector('.button-mode').classList.toggle('button-mode-active')    
-    
-    // Cards ====================================== 
-    let cards = document.querySelectorAll('.card')
-    cards.forEach(card => {
-        card.classList.toggle('card-dark')
-    });
+    //verifica se o modo noturno está habilitado antes ao carregar a página
+    if(darkModeStorage){
+        html.setAttribute("dark", "true");
+        document.querySelector('.button-mode').classList.add('button-mode-active');
+    }
 
-    // Table ======================================
-    let ths = document.querySelectorAll('th');
-    ths.forEach(th => {
-        th.classList.toggle('th-dark')
-    }); 
-
-    const tds = document.querySelectorAll('td');
-    tds.forEach(td => {
-        //adicionar cor aos itens da coluna "description" na tabela
-        if(td.classList.contains('description')){
-            td.classList.toggle('td-description-dark')
+    //lógica para alterar de tema
+    inputDarkMode.addEventListener('change', () => {
+        if(inputDarkMode.checked){
+            html.setAttribute("dark", "true");
+            localStorage.setItem("dark-mode", true);
+            //mudar svg para a versão do modo dark
+            document.querySelector('.button-mode').classList.add('button-mode-active');
+        } else {
+            html.removeAttribute('dark');
+            localStorage.removeItem("dark-mode");
+            //mudar svg para a versão do modo light
+            document.querySelector('.button-mode').classList.remove('button-mode-active');
         }
+    })
+})
 
-        td.classList.toggle('td-dark')
-    });
+//função antiga:
 
-    //Footer =======================================
-    document.querySelector('.text-footer').classList.toggle('text-footer-dark')
+// function darkmode(){
+//     document.querySelector('body').classList.toggle('body-dark');
 
-    //Modal =======================================
-    document.querySelector('.modal').classList.toggle('modal-dark');
-
-    document.querySelector('.title-modal').classList.toggle('title-modal-dark')
+//     document.querySelector('header').classList.toggle('header-dark');
     
-    document.querySelector('small').classList.toggle('inform')
-}
+//     document.querySelector('.button-mode').classList.toggle('button-mode-active')    
+    
+//     // Cards ====================================== 
+//     let cards = document.querySelectorAll('.card')
+//     cards.forEach(card => {
+//         card.classList.toggle('card-dark')
+//     });
+
+//     // Table ======================================
+//     let ths = document.querySelectorAll('th');
+//     ths.forEach(th => {
+//         th.classList.toggle('th-dark')
+//     }); 
+
+//     const tds = document.querySelectorAll('td');
+//     tds.forEach(td => {
+//         //adicionar cor aos itens da coluna "description" na tabela
+//         if(td.classList.contains('description')){
+//             td.classList.toggle('td-description-dark')
+//         }
+
+//         td.classList.toggle('td-dark')
+//     });
+
+//     //Footer =======================================
+//     document.querySelector('.text-footer').classList.toggle('text-footer-dark')
+
+//     //Modal =======================================
+//     document.querySelector('.modal').classList.toggle('modal-dark');
+
+//     document.querySelector('.title-modal').classList.toggle('title-modal-dark')
+    
+//     document.querySelector('small').classList.toggle('inform')
+// }
